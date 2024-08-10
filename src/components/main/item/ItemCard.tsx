da7c234b146faa36e5865c95a61de330941a4165
Oh-Donghan <odonghan94@gmail.com>
2024-08-10 13:27:03 +0900

    add: ItemCard에 임시 navigate 추가

--- src/components/main/item/ItemCard.tsx
+++ src/components/main/item/ItemCard.tsx
@@ -11,10 +11,23 @@ import {
   Flex,
 } from '@chakra-ui/react';
 import Timer from '../timer/Timer';
+import { useNavigate } from 'react-router-dom';
 
 export default function ItemCard() {
+  const navigate = useNavigate();
+
+  const moveDetail = () => {
+    navigate(`/detail`);
+  };
+
   return (
-    <Card borderWidth="1px" borderColor="gray.200" position={'relative'} zIndex={40}>
+    <Card
+      borderWidth="1px"
+      borderColor="gray.200"
+      position={'relative'}
+      zIndex={40}
+      onClick={moveDetail}
+    >
       <CardBody>
         <Image
           src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
