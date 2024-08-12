4c6e3f65ca1fb166bb4b21e7a2fbfd985c1baa3d
Oh-Donghan <odonghan94@gmail.com>
2024-08-12 15:19:42 +0900

    add: Layout컴포넌트 mypage진입시 footer 안보이게 수정, 라우터에 mypage 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './Footer';
 import Nav from './Nav';
 
 export default function Layout() {
+  const location = useLocation();
+
+  // mypage 경로에 있을 때 Footer를 제외
+  const hideFooter = location.pathname === '/mypage';
+
   return (
     <>
       <Nav />
       <Outlet />
-      <Footer />
+      {!hideFooter && <Footer />}
     </>
   );
 }
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
 import Home from '../pages/Home';
 import AuctionDetail from '../pages/AuctionDetail';
+import AuctionMyPage from '../pages/AuctionMyPage';
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: '/detail',
         element: <AuctionDetail />,
+      },
+      {
+        path: '/mypage',
+        element: <AuctionMyPage />,
       },
     ],
   },
