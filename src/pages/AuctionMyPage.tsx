47ff8f5097c77175f15975ec060092ff78a58c0d
Oh-Donghan <odonghan94@gmail.com>
2024-08-12 16:01:32 +0900

    add: 마이페이지 컨텐츠 간격 조정

--- src/pages/AuctionMyPage.tsx
+++ src/pages/AuctionMyPage.tsx
@@ -12,24 +12,45 @@ import {
   UnorderedList,
 } from '@chakra-ui/react';
 import Calendar from '../components/mypage/calendar/Calendar';
+import { FaGear, FaUser } from 'react-icons/fa6';
+import { FaShoppingCart } from 'react-icons/fa';
+import { RiDiscountPercentFill } from 'react-icons/ri';
+import { SiLightning } from 'react-icons/si';
+import { IoAlertCircle } from 'react-icons/io5';
 
 export default function AuctionMyPage() {
   return (
     <Flex className="max-w-screen-xl mx-auto h-h-screen-minus-nav">
-      <Flex width={'250px'} direction={'column'} className="p-4 bg-slate-100 border border-r-black">
+      <Flex
+        width={'250px'}
+        direction={'column'}
+        className="p-6 bg-slate-100 border border-r-black text-lg flex-shrink-0"
+      >
         <List spacing={3}>
-          <ListItem>내 정보</ListItem>
-          <ListItem>구매 내역</ListItem>
-          <ListItem>판매 내역</ListItem>
-          <ListItem>충전 내역</ListItem>
+          <ListItem className="flex items-center gap-2">
+            <FaUser /> <span>내 정보</span>
+          </ListItem>
+          <ListItem className="flex items-center gap-2">
+            <FaShoppingCart /> <span>구매 내역</span>
+          </ListItem>
+          <ListItem className="flex items-center gap-2">
+            <RiDiscountPercentFill /> <span>판매 내역</span>
+          </ListItem>
+          <ListItem className="flex items-center gap-2">
+            <SiLightning /> <span>충전 내역</span>
+          </ListItem>
         </List>
         <Spacer />
         <List spacing={3} className="py-4">
-          <ListItem>비밀번호 변경</ListItem>
-          <ListItem>회원탈퇴</ListItem>
+          <ListItem className="flex items-center gap-2">
+            <FaGear /> <span>비밀번호 변경</span>
+          </ListItem>
+          <ListItem className="flex items-center gap-2">
+            <IoAlertCircle /> <span>회원탈퇴</span>
+          </ListItem>
         </List>
       </Flex>
-      <Box bgColor={'lightgray'} className="py-12 w-full flex justify-center">
+      <Box flex={1} bgColor={'lightgray'} className="p-12 flex justify-center">
         <Flex gap={10}>
           <Avatar width={'120px'} height={'120px'} />
           <Flex direction={'column'} bgColor={'skyblue'} gap={4}>
