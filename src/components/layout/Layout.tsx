dfa5f3f3abdf4482a712812d596b4493eb480657
Oh-Donghan <odonghan94@gmail.com>
2024-08-15 15:01:25 +0900

    add: 마이페이지 영역 레이아웃 및 라우터 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,11 @@ export default function Layout() {
   const location = useLocation();
 
   // mypage 경로에 있을 때 Footer를 제외
-  const hideFooter = location.pathname === '/mypage';
+  const hideFooter =
+    location.pathname === '/mypage/myinfo' ||
+    location.pathname === '/mypage/buy' ||
+    location.pathname === '/mypage/sell' ||
+    location.pathname === '/mypage/charge';
 
   return (
     <>
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,12 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
 import Home from '../pages/Home';
 import AuctionDetail from '../pages/AuctionDetail';
 import AuctionMyPage from '../pages/AuctionMyPage';
 import MyInfo from '../components/mypage/myInfo/MyInfo';
+import PurchaseHistory from '../components/mypage/order/PurchaseHistory';
+import SellHistory from '../components/mypage/order/SellHistory';
+import RechargeHistory from '../components/mypage/order/RechargeHistory';
 
 export const router = createBrowserRouter([
   {
@@ -23,8 +26,24 @@ export const router = createBrowserRouter([
         element: <AuctionMyPage />,
         children: [
           {
-            path: '/mypage/myinfo',
+            path: '',
+            element: <Navigate to="myinfo" replace />,
+          },
+          {
+            path: 'myinfo',
             element: <MyInfo />,
+          },
+          {
+            path: 'buy',
+            element: <PurchaseHistory />,
+          },
+          {
+            path: 'sell',
+            element: <SellHistory />,
+          },
+          {
+            path: 'charge',
+            element: <RechargeHistory />,
           },
         ],
       },
