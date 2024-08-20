9211593319a6b91a449e90bbcbebadb45cc61f16
Oh-Donghan <odonghan94@gmail.com>
2024-08-20 18:47:05 +0900

    feat: 판매 내역 API 호출에 정렬 및 날짜 필터링 파라미터 추가

--- src/axios/mocks/order/sellHistory.ts
+++ src/axios/mocks/order/sellHistory.ts
@@ -1,6 +1,25 @@
 import axiosInstance from '../../instances';
 
-export const fetchSellHistoryData = async () => {
-  const response = await axiosInstance.get('/api/transactions/sales');
-  return response.data.content;
+export const fetchSellHistoryData = async (
+  page: number,
+  itemsPerPage: number,
+  searchWord: string,
+  transType: string,
+  sorted: string,
+  startDate: Date | null,
+  endDate: Date | null,
+) => {
+  const response = await axiosInstance.get('/api/transactions/sales', {
+    params: {
+      page: page - 1, // 페이지는 0부터 시작
+      size: itemsPerPage,
+      word: searchWord || '', // 검색어
+      transTypeString: transType || '', // 거래 상태
+      sorted: sorted || 'recent', // 정렬 기준
+      startDate: startDate ? startDate.toISOString().slice(0, 10) : null, // 시작 날짜
+      endDate: endDate ? endDate.toISOString().slice(0, 10) : null, // 종료 날짜
+    },
+  });
+
+  return response.data;
 };
