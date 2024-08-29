c677d36bd6c85e0c87d64014af96a5eb2e1b6fcc
Oh-Donghan <odonghan94@gmail.com>
2024-08-30 00:59:30 +0900

    fix: axios instance 수정

--- src/axios/instances.ts
+++ src/axios/instances.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_BASE_URL || 'https://fake-server.com',
+  // baseURL: import.meta.env.VITE_BASE_URL,
   baseURL: 'https://dddang.store',
 });
 
 axiosInstance.interceptors.request.use(
   config => {
-    // 개발 환경일 때는 토큰 검사 생략
     if (import.meta.env.MODE !== 'development') {
       // 로컬에서 토큰 불러오기
       const token = localStorage.getItem('accessToken');
