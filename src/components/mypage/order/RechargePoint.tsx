433c1205d35002a421224427d727ef288cd54d18
Oh-Donghan <odonghan94@gmail.com>
2024-08-30 20:33:53 +0900

    feat: 보유 포인트 컴포넌트 생성

---
+++ src/components/mypage/order/RechargePoint.tsx
@@ -0,0 +1,37 @@
+import { Box, Flex, Spinner, Text } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchCurrentPoint } from '../../../axios/auctionDetail/currentPoint';
+
+export default function RechargePoint() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['points'],
+    queryFn: fetchCurrentPoint,
+  });
+
+  console.log(data);
+
+  if (isLoading) {
+    return (
+      <Flex justifyContent="center" alignItems="center" height="100%" width="100%">
+        <Spinner size="xl" />
+      </Flex>
+    );
+  }
+
+  if (isError) {
+    return <div>Error loading data</div>;
+  }
+
+  return (
+    <Flex
+      justifyContent={'end'}
+      alignItems={'end'}
+      gap={4}
+      marginRight={'24px'}
+      marginBottom={'8px'}
+    >
+      <Text fontSize={{ base: 'sm', md: 'xl' }}>보유 포인트</Text>
+      <Box fontSize={{ base: 'lg', md: '3xl' }}>{data?.pointAmount}P</Box>
+    </Flex>
+  );
+}
