6030d4d0b05c2e9cb4642b7236dce7b1c99007f4
Oh-Donghan <odonghan94@gmail.com>
2024-08-31 10:04:19 +0900

    update: 충전 내역 및 테이블 수정

--- src/components/mypage/order/RechargeTable.tsx
+++ src/components/mypage/order/RechargeTable.tsx
@@ -6,8 +6,10 @@ export default function RechargeTable({ posts }) {
   const tdPadding = useBreakpointValue({ base: '8px', md: '12px', lg: '16px' }); // 반응형 패딩 설정
 
   if (!posts || posts.length === 0) {
-    return <div>구매 목록이 없습니다.</div>;
+    return <div>거래 내역이 없습니다.</div>;
   }
+
+  console.log(posts);
 
   return (
     <div className="overflow-y-scroll no-scrollbar h-full max-sm:h-72">
@@ -20,12 +22,20 @@ export default function RechargeTable({ posts }) {
                 fontWeight={'bold'}
                 textAlign={'center'}
                 className={`${
-                  item.pointType === 'USE' ? 'bg-white text-black' : 'bg-black text-white'
-                }`}
+                  item.pointType === 'USE'
+                    ? 'bg-red-500 text-white'
+                    : item.pointType === 'CHARGE'
+                      ? 'bg-green-500 text-white'
+                      : 'bg-blue-500 text-white'
+                }`} // pointType에 따른 배경색 및 텍스트 색상 변경
                 fontSize={fontSize} // 반응형 폰트 사이즈 적용
                 padding={tdPadding} // 반응형 패딩 적용
               >
-                {item.pointType === 'USE' ? '사용' : '충전'}
+                {item.pointType === 'USE'
+                  ? '사용'
+                  : item.pointType === 'CHARGE'
+                    ? '충전'
+                    : '판매 수익'}
               </Td>
               <Td padding={tdPadding}>
                 <Box display="flex" justifyContent="space-between" fontSize={fontSize}>
