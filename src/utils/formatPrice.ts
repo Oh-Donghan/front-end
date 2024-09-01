4dc130e55b608c257554c75fd1648d06881efb96
Oh-Donghan <odonghan94@gmail.com>
2024-09-01 16:53:50 +0900

    update: 유틸함수 조건문으로 데이터 검사

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,3 +1,6 @@
 export const formatPrice = (price?: number) => {
+  if (price === undefined || price === null) {
+    return '0'; // 기본값을 0으로 반환하거나, 적절한 대체 텍스트 반환
+  }
   return price.toLocaleString();
 };
