910eb523bd099bfb3500c084fa8dd0ff6bd3a69d
Oh-Donghan <odonghan94@gmail.com>
2024-09-01 16:51:53 +0900

    update: QnA영역 무한스크롤을 위한 UI 수정

--- src/components/mypage/order/QnA.tsx
+++ src/components/mypage/order/QnA.tsx
@@ -1,22 +1,28 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import MyQnA from './MyQnA';
 import ReceivedQnA from './ReceivedQnA';
 
 export default function QnA() {
   return (
-    <Tabs isFitted variant="enclosed">
-      <TabList mb="1em" fontWeight="bold">
-        <Tab>내 질문 조회</Tab>
-        <Tab>받은 질문 조회</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <MyQnA />
-        </TabPanel>
-        <TabPanel>
-          <ReceivedQnA />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <Box w="100%" h="100vh" overflow="hidden">
+      {/* 부모 요소에 100vh로 높이 설정 */}
+      <Tabs isFitted variant="enclosed" h="100%">
+        {/* Tabs 컴포넌트에도 높이 설정 */}
+        <TabList fontWeight="bold">
+          <Tab>내 질문 조회</Tab>
+          <Tab>받은 질문 조회</Tab>
+        </TabList>
+        <TabPanels h="calc(100% - 3em)">
+          {/* TabList 높이를 제외한 나머지 영역을 TabPanels가 차지 */}
+          <TabPanel h="100%" overflowY="auto">
+            {/* TabPanel도 전체 높이를 차지하고 내부 스크롤 */}
+            <MyQnA />
+          </TabPanel>
+          <TabPanel h="100%" overflowY="auto">
+            <ReceivedQnA />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
   );
 }
