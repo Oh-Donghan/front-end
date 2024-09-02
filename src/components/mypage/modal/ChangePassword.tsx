2ea70a2ae18006518eff9744b620ed0c8a17ee74
Oh-Donghan <odonghan94@gmail.com>
2024-09-02 22:17:38 +0900

    feat: 비밀번호 변경 모달 구현

--- src/components/mypage/modal/ChangePassword.tsx
+++ src/components/mypage/modal/ChangePassword.tsx
@@ -10,19 +10,96 @@ import {
   ModalHeader,
   ModalOverlay,
   Stack,
+  useToast,
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import { fetchChangePassword } from '../../../axios/mypage/changepassword';
 
 export default function ChangePassword({ onClose, isOpen, initialRef }) {
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [changePassword, setChangePassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const toast = useToast();
+  const [passwords, setPasswords] = useState({
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+  });
 
-  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setPasswords(prevState => ({
+      ...prevState,
+      [id]: value, // id를 키로 사용하여 해당 입력 필드의 값을 업데이트
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log('current: ', currentPassword);
-    console.log('change: ', changePassword);
-    console.log('confirm: ', confirmPassword);
+    console.log('Current Password:', passwords.currentPassword);
+    console.log('New Password:', passwords.newPassword);
+    console.log('Confirm Password:', passwords.confirmPassword);
+    // 여기에 비밀번호 변경 로직을 추가하세요.
+    if (
+      passwords.currentPassword === '' ||
+      passwords.newPassword === '' ||
+      passwords.confirmPassword === ''
+    ) {
+      toast({
+        title: '양식을 전부 채워주세요.',
+        status: 'warning',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+    if (passwords.currentPassword === passwords.newPassword) {
+      toast({
+        title: '변경할 비밀번호가 현재 비밀번호와 동일합니다.',
+        status: 'warning',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+    if (passwords.newPassword !== passwords.confirmPassword) {
+      toast({
+        title: '변경할 비밀번호와 비밀번호 확인이 일치하지 않습니다.',
+        status: 'warning',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    const response = await fetchChangePassword({
+      curPassword: passwords.currentPassword,
+      newPassword: passwords.newPassword,
+      confirmPassword: passwords.confirmPassword,
+    });
+
+    if (response) {
+      toast({
+        title: '비밀번호가 변경 되었습니다.',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+      handleReset();
+    } else {
+      toast({
+        title: '비밀번호 변경 실패.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleReset = () => {
+    onClose();
+    setPasswords({
+      currentPassword: '',
+      newPassword: '',
+      confirmPassword: '',
+    });
   };
 
   return (
@@ -41,32 +118,26 @@ export default function ChangePassword({ onClose, isOpen, initialRef }) {
         <ModalBody>
           <FormControl>
             <Stack spacing={2}>
-              <FormLabel htmlFor="current" margin={0}>
+              <FormLabel htmlFor="currentPassword" margin={0}>
                 현재 비밀번호
               </FormLabel>
               <Input
-                id="current"
+                id="currentPassword"
                 ref={initialRef}
-                value={currentPassword}
-                onChange={e => setCurrentPassword(e.target.value)}
+                value={passwords.currentPassword}
+                onChange={handleChange}
               />
-              <FormLabel htmlFor="change" margin={0}>
+              <FormLabel htmlFor="newPassword" margin={0}>
                 변경할 비밀번호
               </FormLabel>
-              <Input
-                id="change"
-                ref={initialRef}
-                value={changePassword}
-                onChange={e => setChangePassword(e.target.value)}
-              />
-              <FormLabel htmlFor="confirm" margin={0}>
+              <Input id="newPassword" value={passwords.newPassword} onChange={handleChange} />
+              <FormLabel htmlFor="confirmPassword" margin={0}>
                 한번 더 입력
               </FormLabel>
               <Input
-                id="confirm"
-                ref={initialRef}
-                value={confirmPassword}
-                onChange={e => setConfirmPassword(e.target.value)}
+                id="confirmPassword"
+                value={passwords.confirmPassword}
+                onChange={handleChange}
               />
               <span className="text-xs">
                 4자 ~ 16자 이내의 숫자, 특수문자, 영문자 중 2가지 이상을 조합
@@ -75,11 +146,11 @@ export default function ChangePassword({ onClose, isOpen, initialRef }) {
           </FormControl>
         </ModalBody>
         <ModalFooter className="flex gap-4">
-          <Button className="w-full" bgColor={'white'} onClick={onClose}>
-            취소
-          </Button>
-          <Button className="w-full" onClick={handleSubmit}>
+          <Button className="w-full" colorScheme="blue" onClick={handleSubmit}>
             변경
+          </Button>
+          <Button className="w-full" bgColor={'white'} onClick={handleReset}>
+            취소
           </Button>
         </ModalFooter>
       </ModalContent>
