1b4e77d57baa3f8a3bdc2be58e7a29baca20a51c
Oh-Donghan <odonghan94@gmail.com>
2024-09-03 04:14:59 +0900

    update: 비밀번호 틀릴시 경고 메시지 추가 및 비밀번호 숨김 표시

--- src/components/mypage/modal/ChangePassword.tsx
+++ src/components/mypage/modal/ChangePassword.tsx
@@ -24,10 +24,15 @@ import { useRef, useState } from 'react';
 import { fetchChangePassword } from '../../../axios/mypage/changepassword';
 import { useQuery } from '@tanstack/react-query';
 import { fetchMemberInquiry } from '../../../axios/mypage/memberinquiry';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from '../../../axios/auth/user';
 
 export default function ChangePassword({ onClose, isOpen, initialRef }) {
   const toast = useToast();
   const cancelRef = useRef();
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+
   const [passwords, setPasswords] = useState({
     currentPassword: '',
     newPassword: '',
@@ -47,6 +52,12 @@ export default function ChangePassword({ onClose, isOpen, initialRef }) {
     );
   }
 
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
+  const handleClick = () => setShow(!show);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
     setPasswords(prevState => ({
@@ -57,10 +68,7 @@ export default function ChangePassword({ onClose, isOpen, initialRef }) {
 
   const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log('Current Password:', passwords.currentPassword);
-    console.log('New Password:', passwords.newPassword);
-    console.log('Confirm Password:', passwords.confirmPassword);
-    // 여기에 비밀번호 변경 로직을 추가하세요.
+
     if (
       passwords.currentPassword === '' ||
       passwords.newPassword === '' ||
@@ -93,23 +101,39 @@ export default function ChangePassword({ onClose, isOpen, initialRef }) {
       return;
     }
 
-    const response = await fetchChangePassword({
-      curPassword: passwords.currentPassword,
-      newPassword: passwords.newPassword,
-      confirmPassword: passwords.confirmPassword,
-    });
-
-    if (response) {
-      toast({
-        title: '비밀번호가 변경 되었습니다.',
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
+    try {
+      const response = await fetchChangePassword({
+        curPassword: passwords.currentPassword,
+        newPassword: passwords.newPassword,
+        confirmPassword: passwords.confirmPassword,
       });
-      handleReset();
-    } else {
+
+      if (response) {
+        toast({
+          title: '비밀번호가 변경 되었습니다.',
+          status: 'success',
+          duration: 3000,
+          isClosable: true,
+        });
+        // 로컬스토리지에서 accessToken과 memberId 삭제
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('memberId');
+
+        handleReset();
+        handleSignOut();
+        navigate('/');
+      } else {
+        toast({
+          title: '비밀번호 변경 실패.',
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      console.log(error.response.data);
       toast({
-        title: '비밀번호 변경 실패.',
+        title: error.response.data,
         status: 'error',
         duration: 3000,
         isClosable: true,
@@ -141,14 +165,21 @@ export default function ChangePassword({ onClose, isOpen, initialRef }) {
             <ModalHeader fontSize={'2xl'} textAlign={'center'}>
               비밀번호 변경
             </ModalHeader>
+            <Flex justifyContent="flex-end">
+              <Button mr="24px" onClick={handleClick}>
+                {show ? '숨기기' : '보이기'}
+              </Button>
+            </Flex>
             <ModalBody>
               <FormControl>
                 <Stack spacing={2}>
                   <FormLabel htmlFor="currentPassword" margin={0}>
                     현재 비밀번호
                   </FormLabel>
+                  <Flex justifyContent="flex-end"></Flex>
                   <Input
                     id="currentPassword"
+                    type={show ? 'text' : 'password'}
                     ref={initialRef}
                     value={passwords.currentPassword}
                     onChange={handleChange}
@@ -156,18 +187,21 @@ export default function ChangePassword({ onClose, isOpen, initialRef }) {
                   <FormLabel htmlFor="newPassword" margin={0}>
                     변경할 비밀번호
                   </FormLabel>
-                  <Input id="newPassword" value={passwords.newPassword} onChange={handleChange} />
+                  <Input
+                    id="newPassword"
+                    type={show ? 'text' : 'password'}
+                    value={passwords.newPassword}
+                    onChange={handleChange}
+                  />
                   <FormLabel htmlFor="confirmPassword" margin={0}>
                     한번 더 입력
                   </FormLabel>
                   <Input
                     id="confirmPassword"
+                    type={show ? 'text' : 'password'}
                     value={passwords.confirmPassword}
                     onChange={handleChange}
                   />
-                  <span className="text-xs">
-                    4자 ~ 16자 이내의 숫자, 특수문자, 영문자 중 2가지 이상을 조합
-                  </span>
                 </Stack>
               </FormControl>
             </ModalBody>
