7a67e12ac97aead8425a3daf58d2aedf8262f6b2
DESKTOP-36S7C96\최지영 <chldls153@gmail.com>
2024-09-03 21:31:53 +0900

    fix: 회원가입 아이디 중복확인 시 이미 가입된 아이디 에러 처리

--- src/components/main/modals/auth/SignupModal.tsx
+++ src/components/main/modals/auth/SignupModal.tsx
@@ -162,17 +162,15 @@ export default function SignupModal({ onClose, isOpen, initialRef }) {
                 }
                 try {
                   const response = await idDuplicateCheck({ id });
-                  setIdMessage(response);
-                  setIdMessageColor('rgba(0, 119, 255, 0.9)');
+
                   if (response === '사용 가능한 아이디 입니다.') {
-                    setIsIdConfirmed(true);
-                  } else if (response === '이미 존재하는 아이디입니다. ') {
-                    setIsIdConfirmed(false);
-                    toast({
-                      title: '이미 존재하는 아이디입니다.',
-                      status: 'error',
-                      duration: 1300,
-                    });
+                    await setIsIdConfirmed(true);
+                    await setIdMessage(response);
+                    await setIdMessageColor('rgba(0, 119, 255, 0.9)');
+                  } else if (response === '이미 존재하는 아이디입니다.') {
+                    await setIsIdConfirmed(false);
+                    await setIdMessage(response);
+                    await setIdMessageColor('rgb(240, 33, 33)');
                   }
                 } catch (error) {
                   console.error('아이디 중복 확인 오류 :' + error);
