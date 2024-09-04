781f27196faed3ab998082b4843646beb8d193ce
Oh-Donghan <odonghan94@gmail.com>
2024-09-04 15:10:08 +0900

    update: 즉시 구매시 데이터 새로 불러온 후 메인화면으로 이동 및 새로고침

--- src/components/detail/InstantBuyForm.tsx
+++ src/components/detail/InstantBuyForm.tsx
@@ -1,12 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchInstantBuyData } from '../../axios/auctionDetail/InstantBuy';
 import { Button, useToast } from '@chakra-ui/react';
 import { AxiosError } from 'axios';
 import { authState } from '../../recoil/atom/authAtom';
 import { useRecoilState } from 'recoil';
+import { useNavigate } from 'react-router-dom';
 
 const InstantBuyForm = ({ auctionState, auctionId }) => {
   const [auth] = useRecoilState(authState);
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const toast = useToast();
 
   // useMutation 훅을 사용하여 API 요청을 관리
@@ -20,6 +23,12 @@ const InstantBuyForm = ({ auctionState, auctionId }) => {
         duration: 3000,
         isClosable: true,
       });
+
+      // 즉시 구매시 쿼리를 무효화하여 데이터를 새로고침
+      queryClient.invalidateQueries({ queryKey: ['detail', auctionId] });
+      // 홈으로 이동하고 새로고침
+      navigate('/', { replace: true });
+      window.location.reload();
     },
     onError: (error: AxiosError) => {
       // AxiosError 타입으로 에러 객체 캐스팅
