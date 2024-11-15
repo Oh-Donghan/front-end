fe0b6470e75422c1935b6d651b13c9dcae77d1d9
Oh-Donghan <odonghan94@gmail.com>
2024-11-15 13:50:55 +0900

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider, customTheme } from './config/ChakraProvider';
 import './index.css';
-import { worker } from './mocks/browser';
 import RecoilProvider from './recoil/store';
 
 if (import.meta.env.MODE === 'development') {
-  worker.start({
-    onUnhandledRequest: 'bypass', // 처리되지 않은 요청을 무시하고 네트워크로 전달
+  import('./mocks/browser').then(({ worker }) => {
+    worker.start({
+      onUnhandledRequest: 'bypass', // 처리되지 않은 요청을 네트워크로 전달
+    });
   });
 }
 
